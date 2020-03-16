cf5c398b7e8fb95fc2c627d883f9a9e7a0e54397
Darian Bhathena <darianb@mit.edu>
2020-03-16 15:42:11 -0400

    MongoDB app integration

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// app.js
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Long live BBlack panther club'));
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8082;
 
