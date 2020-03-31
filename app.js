b983dd5336797aaacaa4e41747f220f15e99e438
Darian Bhathena <darianb@mit.edu>
2020-03-31 00:54:44 -0400

    another attempt at socket fix

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const app = express();
-const server = app.listen();
-const io = require('socket.io').listen(server).sockets;
+const socketIO = require('socket.io');
 
 // routes
 const users = require('./routes/api/user');
 const bunkers = require('./routes/api/bunker');
 const measures = require('./routes/api/measure');
 
-// Connect Database
-connectDB();
+// app and socket setup
+const port = process.env.PORT || 8082;
+const app = express();
 
+app.listen(port, () => console.log(`Server running on port ${port}`));
+const io = socketIO(app);
 io.on('connection', function(socket){
     console.log("Socket connected...");
 });
-
 app.io = io;
+
+// Connect Database
+connectDB();
 
 // use cors and parser
 app.use(cors({ origin: true, credentials: true }));
@@ -33,6 +35,3 @@ app.use('/api/users', users);
 app.use('/api/bunkers', bunkers);
 app.use('/api/measures', measures);
 
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
